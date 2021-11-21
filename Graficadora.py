326e788245a86314284098c9b9cd8dcf32e1d549
nietonicolasleon <91757311+nietonicolasleon@users.noreply.github.com>
2021-11-21 14:32:55 -0300

    Versión mejorada de Graficadora.py y de la imagen
    Ahora en Graficadora.py se usan colores oscuros para que se vean las letras blancas. También se subió una nueva imagen, que no está en blanco como la anterior.

--- Graficadora.py
+++ Graficadora.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot
 from ObtenerDatos import consola, jugadores
 
-colores = ('red', 'blue', 'cyan', 'green')  # Colores que se van a aplicar en el gráfico
+colores = ('red', 'blue', 'black', 'green')  # Colores que se van a aplicar en el gráfico
 
 pyplot.rcParams['toolbar'] = 'None'  # Oculta la toolbar de pyplot
 
@@ -13,4 +13,4 @@ for tex in texto:
 pyplot.axis('equal')  # Convierte al gráfico en un círuclo perfecto
 pyplot.title('Gráfico circular de la cantidad de jugadores por consola')  # Agrega un título
 pyplot.show()  # Muestra el gráfico
-pyplot.savefig('GráficoQueryB.png')  # Guarda el gráfico como imagen
+pyplot.savefig('graficoqueryb.png')  # Guarda el gráfico como imagen
