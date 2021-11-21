b940aef64e6b05bccb883ed5d429172cd1632943
nietonicolasleon <91757311+nietonicolasleon@users.noreply.github.com>
2021-11-21 14:17:16 -0300

    Nueva versión de Graficadora.py y gráfico
    Se documentó el código de Graficadora, agregué la llamada a función para que se guarde la imagen y subí la imagen a modo de muestra.

--- Graficadora.py
+++ Graficadora.py
@@ -1,15 +1,16 @@
 from matplotlib import pyplot
 from ObtenerDatos import consola, jugadores
 
-colores = ('red', 'blue', 'cyan', 'green')
+colores = ('red', 'blue', 'cyan', 'green')  # Colores que se van a aplicar en el gráfico
 
-pyplot.rcParams['toolbar'] = 'None'
+pyplot.rcParams['toolbar'] = 'None'  # Oculta la toolbar de pyplot
 
-_, _, texto = pyplot.pie(jugadores, colors=colores, labels=consola, autopct='%1.1f%%', startangle=90)
+_, _, texto = pyplot.pie(jugadores, colors=colores, labels=consola, autopct='%1.1f%%', startangle=90)  # Instrucciones para generar el gráfico
 
 for tex in texto:
-    tex.set_color('white')
+    tex.set_color('white')  # Cambia las letras dentro de los colores a blanco
 
-pyplot.axis('equal')
-pyplot.title('Gráfico circular de la cantidad de jugadores por consola')
-pyplot.show()
+pyplot.axis('equal')  # Convierte al gráfico en un círuclo perfecto
+pyplot.title('Gráfico circular de la cantidad de jugadores por consola')  # Agrega un título
+pyplot.show()  # Muestra el gráfico
+pyplot.savefig('GráficoQueryB.png')  # Guarda el gráfico como imagen
